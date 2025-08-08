2eabe7411f58df883a885ef87fc5074fafce2469
Nev Senkrov <nevsenkrov@gmail.com>
2025-08-08 00:15:00 -0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -17,7 +17,9 @@ export const POSTS: Post[] = [
     short: 'experience desciptor',
     full: `full detailed report of the experience
 
-    you can also just use plain text next line to format by text blocks btw`,
+    you can also just use plain text next line to format by text blocks btw
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
     category: Category.Experience,
     tag: 'EXPERIENCE.000',
   },
