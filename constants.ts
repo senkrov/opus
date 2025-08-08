a4b7ee1245ce3e4d71d44838457c01c5e1f77c7e
Nev Senkrov <nevsenkrov@gmail.com>
2025-08-07 23:20:00 -0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const POSTS: Post[] = [
     id: 0,
     title: 'PROJECT OPUS',
     short: 'all the cool kids have personal websites, I wanted one too',
-    full: `everything else`,
+    full: `started conceptualizing with replit, used screenshots to remake the app based on represented styles and additional developed prompts in Google's AI Studio using Gemini 2.5 Pro`,
     category: Category.Effort,
     tag: 'EFFORT.000',
   },
